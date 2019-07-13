186ba0d3f22307fa1c0c8af005c9e2de027b9d10
vineethsomanchi <VineethSomanchi@gmail.com>
2019-07-13 15:05:50 -0400

    Finish two_sum and init commit for amazon interview practice

---
+++ amazon/two_sum.rb
@@ -0,0 +1,12 @@
+def two_sum(arr, target)
+    hash = {}
+    arr.each_with_index do |ele, idx|
+        if hash[target - ele]
+            return [hash[target - ele], idx]
+        else
+            hash[ele] = idx
+        end
+    end
+end
+
+puts two_sum([2, 1, 11, 7], 9)
