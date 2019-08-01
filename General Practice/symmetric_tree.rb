89c282abb5df75961f533bdaa1c98afcd70710aa
vineethsomanchi <VineethSomanchi@gmail.com>
2019-07-31 18:56:46 -0700

    Symmetric Tree

---
+++ General Practice/symmetric_tree.rb
@@ -0,0 +1,10 @@
+def is_symmetric(root)
+    return true if root.nil?
+    is_mirror(root.left, root.right)
+end
+
+def is_mirror(a, b)
+    return a == b if a.nil? || b.nil?
+    return false  if a.val != b.val
+    is_mirror(a.left, b.right) && is_mirror(a.right, b.left)
+end
